1f97203e49be6e0e64048dbe44cd44b33284e8b5
jewelry <supirs102@gmail.com>
2018-11-30 13:23:00 +0900

    headerのnavの隙間の違和感なくす headerのnavのホバー時に日本語に切り替わる挙動追加

--- js/main.js
+++ js/main.js
@@ -300,13 +300,13 @@ $(function(){
 
     // headernavhover
     var nav = $('#navya');
-    nav.append('<span></span>');
+    nav.append('<span class="navsankaku"></span>');
     //原点の位置
     var contY = nav.offset().top;
     $('li a', nav).mouseover(function(){  
         //移動先の位置を取得
-        var thisOffset = $(this).offset().top - contY - 12;
-        $('span', nav).stop().animate({
+        var thisOffset = $(this).offset().top - contY - 8;
+        $('span.navsankaku', nav).stop().animate({
             top : thisOffset +'px'
         },200);
     });
