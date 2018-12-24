b3e8235514dc404d3c81e89cc76f83d238c038bb
jewelry <supirs102@gmail.com>
2018-12-24 23:40:54 +0900

    mastercheff-tbのクラッシュのためのdiv追加 全端末ピンチ禁止

--- js/main.js
+++ js/main.js
@@ -475,5 +475,20 @@ $(function(){
 		}
 	});
 
+	// ピンチ禁止
+	/* ピッチインピッチアウトによる拡大縮小を禁止 */
+	document.documentElement.addEventListener('touchstart', function (e) {
+	  if (e.touches.length >= 2) {e.preventDefault();}
+	}, {passive: false});
+	/* ダブルタップによる拡大を禁止 */
+	var t = 0;
+	document.documentElement.addEventListener('touchend', function (e) {
+		var now = new Date().getTime();
+		if ((now - t) < 350){
+	    	e.preventDefault();
+		}
+		t = now;
+	}, false);
+
 });
 
